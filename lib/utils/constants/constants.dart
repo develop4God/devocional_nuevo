4b442a5b882d6a2b8d4bbaf558d8d3437e3c65bb
develop4God <develop4God@gmail.com>
2026-06-20 20:16:46 -0500

    Remove ESV from available English Bible versions
    - Remove 'ESV' from the English language list in `bibleVersionsByLanguage` within `constants.dart`
    - Update comments for Spanish and English entries to indicate that NTV and ESV are not yet available in the drawer menu

--- lib/utils/constants/constants.dart
+++ lib/utils/constants/constants.dart
@@ -67,8 +67,8 @@ class Constants {
 
   // Available versions by language / Drawer menu options (SRP: single source of truth for all version lists)
   static const Map<String, List<String>> bibleVersionsByLanguage = {
-    'es': ['RVR1960', 'NVI'],//NTV not available on drawer yet
-    'en': ['KJV', 'NIV', 'ESV'],
+    'es': ['RVR1960', 'NVI'],//> ,'NTV' not available on drawer yet
+    'en': ['KJV', 'NIV' ], //> ,'ESV'not available on drawer yet
     'pt': ['ARC', 'NVI'],
     'fr': ['LSG1910', 'TOB'],
     'ja': ['新改訳2003', 'リビングバイブル'], // Japanese versions
